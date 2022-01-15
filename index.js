7f1cb04a81520832682d3c3619dc460c0cf84284
Jeff Sulfranc <sulfranc@gmail.com>
2022-01-15 16:31:25 -0500

    commit tree f7b166e197e0d0e3829f1e62cef0889891bbfd2f

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer;
+const leastFavoriteCustomer = 'john'
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+     bestCustomer = 'not bob';
+
+
+}
+
+function overwriteBestCustomer(){
+     bestCustomer = 'maybe bob' ;
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'bill'
+}
